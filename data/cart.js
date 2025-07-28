50d7b743c535fb8c50dac6503da11a8b34bdd70a
hammad khalid <hammadkhalid607@gmail.com>
2025-07-28 22:56:42 +0500

    save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,22 +1,17 @@
-export let cart = JSON.parse(localStorage.getItem("cart"));
+export let cart = JSON.parse(localStorage.getItem("cart")) ||
   
-  //  [
-  //     {
-  //       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  //       quantity: 1,
-    
-  //     },
-  //     {
-  //       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  //       quantity: 2,
-    
-  //     },
-  //     {
-  //       productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-  //       quantity: 3,
-    
-  //     }
-  //   ];
+   [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1,
+        deliveryOptionId :'1'   
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 2,
+        deliveryOptionId : '3'
+      }
+    ];
 
 
 
@@ -43,6 +38,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId : '1'
     });
   }
   saveToStorage();
